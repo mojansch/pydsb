df5207c5f444e41bfbee8ea7cc1a8f14fc0f8509
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-08-20 19:40:08 +0200

    added login method

--- pydsb.py
+++ pydsb.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class PyDSB:
     URL = "https://iphone.dsbcontrol.de/iPhoneService.svc/DSB"
 
@@ -5,3 +8,16 @@ class PyDSB:
         self.username = username
         self.password = password
         self.token = None
+
+    def login(self):
+        req = requests.get(f"{self.URL}/authid/{self.username}/{self.password}")
+        token = req.text.strip("\"")
+
+        if token == "00000000-0000-0000-0000-000000000000":
+            raise LoginError("Wrong username or password")
+
+        self.token = token
+
+
+class LoginError(Exception):
+    pass
