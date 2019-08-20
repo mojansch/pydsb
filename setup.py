a2bbf9c11d837fd7010f2a1ebae9663833451abd
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-08-20 19:15:21 +0200

    created setup.py + pydsb.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="pydsb",
+      version="0.1",
+      description="Python API for DSBmobile",
+      url="https://github.com/sn0wmanmj/pydsb",
+      author="Moritz Jannasch",
+      author_email="contact@moritzj.de",
+      license="MIT",
+      packages=["pydsb"]
+      )
