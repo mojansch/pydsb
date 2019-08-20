fc563a5fbf8e4db9168fbc3b85d7a479723f3d0e
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-08-20 20:41:22 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-from setuptools import setup
+import setuptools
 
-setup(name="pydsb",
-      version="0.2",
-      description="Python API for DSBmobile",
-      url="https://github.com/sn0wmanmj/pydsb",
-      author="Moritz Jannasch",
-      author_email="contact@moritzj.de",
-      license="MIT",
-      packages=["pydsb"],
-      install_requires=[
-          "requests"
-      ])
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name="pydsb",
+                 version="0.2",
+                 description="Python API for DSBmobile",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/sn0wmanmj/pydsb",
+                 author="Moritz Jannasch",
+                 author_email="contact@moritzj.de",
+                 license="MIT",
+                 packages=setuptools.find_packages(),
+                 install_requires=[
+                     "requests"
+                 ],
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 )
