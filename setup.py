d94aab53cf760fb6fd1afedfc1b2e0f52c7d1a70
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-08-20 20:29:51 +0200

    incremented version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="pydsb",
-      version="0.1",
+      version="0.2",
       description="Python API for DSBmobile",
       url="https://github.com/sn0wmanmj/pydsb",
       author="Moritz Jannasch",
