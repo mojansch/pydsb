c1df2005c714b2f743dfee11716780fb134f3075
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-08-21 17:55:03 +0200

    incremented version to 1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name="pydsb",
-                 version="1.0",
+                 version="1.1",
                  description="Python API for DSBmobile",
                  long_description=long_description,
                  long_description_content_type="text/markdown",
